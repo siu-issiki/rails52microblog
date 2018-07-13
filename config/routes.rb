61bcd19bf1b0e8e75bd855dd6eef9a56138e45b3
siu <siu.issiki@gmail.com>
2018-07-13 16:45:41 +0900

    authorization

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+
   get '/about', to: 'static_pages#about'
   get '/help', to: 'static_pages#help'
   get '/signup', to: 'users#new'
+  post '/signup',  to: 'users#create'
+
+  resources :users
 end
