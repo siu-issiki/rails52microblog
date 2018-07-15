4ab24e00c4d010e1c2a7108b814d7f8ec295f7f7
siu <siu.issiki@gmail.com>
2018-07-15 21:49:58 +0900

    small fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user,     only: :destroy
 
   def index
     @users = User.where(activated: true).paginate(page: params[:page])
@@ -41,9 +40,12 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User deleted"
-    redirect_to users_url
+    @user = User.find(params[:id])
+    if current_user?(@user) || current_user.admin?
+      @user.destroy
+      flash[:success] = "User deleted"
+      redirect_to current_user.admin? ? users_url : root_path
+    end
   end
 
   private
