9ed3e89d1f36af99b5390cd6bfdbd1eb3c63054d
siu <siu.issiki@gmail.com>
2018-07-16 01:16:52 +0900

    マイクロポストcreate時のflash削除

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -5,7 +5,6 @@ class MicropostsController < ApplicationController
   def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
-      flash[:success] = "Micropost created!"
       redirect_to root_url
     else
       render 'static_pages/home'
